09abb92eae47ab8a3ec91d5cafff80bb0c5c2b8c
MorrisMA <morrisma@mchsi.com>
2017-07-25 07:50:15 -0500

    Added Pascal program compactor. Corrected error in ScannerPascal.c -- ignored the print_flag.

---
+++ Compactor-Pascal/Debug/src/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/Compactor-Pascal.c \
+../src/ScannerPascal.c \
+../src/error.c 
+
+OBJS += \
+./src/Compactor-Pascal.o \
+./src/ScannerPascal.o \
+./src/error.o 
+
+C_DEPS += \
+./src/Compactor-Pascal.d \
+./src/ScannerPascal.d \
+./src/error.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
